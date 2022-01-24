6ba932555e6ea64285badab7ae01087c9afbb597
Aaron Stannard <aaron@petabridge.com>
2022-01-23 21:40:21 -0600

    working on rendering output

--- src/OpenTelemetry.Contrib.Testing.Tests/TopologicalSortSpecs.cs
+++ src/OpenTelemetry.Contrib.Testing.Tests/TopologicalSortSpecs.cs
@@ -5,6 +5,8 @@ using System.Diagnostics;
 using System.Linq;
 using Faker.Helpers;
 using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentAssertions.Formatting;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -23,7 +25,7 @@ namespace OpenTelemetry.Contrib.Testing.Tests
         public TraceFixture Traces { get; }
 
         [Fact]
-        public void TopologicalSortSanityCheck()
+        public void BasicCase()
         {
             // arrange
             List<ActivitySpanId> expectedOrder = new();
@@ -54,6 +56,47 @@ namespace OpenTelemetry.Contrib.Testing.Tests
             // assert
             shuffled.Should().NotBeEmpty(); // can't be empty
             sorted.Should().Equal(expected);
+        }
+
+        [Fact]
+        public void MultipleChildren()
+        {
+            // arrange
+            List<ActivitySpanId> expectedOrder = new();
+
+            using (var root = Traces.Tracer.StartActiveSpan("root"))
+            {
+                expectedOrder.Add(root.Context.SpanId);
+                using (var parent = Traces.Tracer.StartActiveSpan("parent"))
+                {
+                    expectedOrder.Add(parent.Context.SpanId);
+                    foreach (var i in Enumerable.Range(0, 10))
+                        using (var child = Traces.Tracer.StartActiveSpan($"child-{i}"))
+                        {
+                            expectedOrder.Add(child.Context.SpanId);
+                            child.AddEvent("hit");
+                        }
+                }
+            }
+
+            // act
+            Traces.ForceFlush();
+            var completed = Traces.CompletedActivities.ToDictionary(c => c.Context.SpanId, a => a);
+            var expected = expectedOrder.Select(c => completed[c]).ToList();
+
+            // use some fisher-yates shuffle to randomize the list
+            var shuffled = Traces.CompletedActivities.Shuffle().ToList();
+            var sorted = shuffled.OrderBy(c => c, TopologicalSort.RelationshipComparer.Instance).ToList();
+
+            // assert
+            shuffled.Should().NotBeEmpty(); // can't be empty
+            using (var scope = new AssertionScope())
+            {
+                // need to print out everything
+                scope.FormattingOptions.MaxLines = Int32.MaxValue;
+                sorted.Should().Equal(expected);
+            }
+            
         }
 
         public void Dispose()
